c1eb0e77568be182631e2d3bfc813a0021ccd7a9
Ruf.Jochen <Ruf.Jochen@SK-448A5BCB8A89>
2015-10-16 08:46:26 +0200

    Update 16.10.2015

--- animation game/src/SimpleOval.java
+++ animation game/src/SimpleOval.java
@@ -7,10 +7,15 @@ public class SimpleOval extends GeometricObject{
 	public SimpleOval (double w,double h,double x, double y){
 		super(w,h,new Vertex(x,y));
 	}
-	@Override public double area() {return Math.PI*width*height/4;}
+	
+	@Override public double area()
+	{return Math.PI*width*height/4;
+	}
+	
 	public @Override String toString() {
 	return "SimpleOval ("+super. toString () +") ";
 	}
+	
 	public @Override boolean equals(Object that) {
 	return (that instanceof SimpleOval) && super.equals(that);
 	}
--- animation game/src/testGeometricObject.java
+++ animation game/src/testGeometricObject.java
@@ -3,9 +3,7 @@ public class testGeometricObject {
 	public static void main(String[] args) {
 		GeometricObject o1 = new GeometricObject(17,4, new Vertex(42,23));
 		GeometricObject o2 = new GeometricObject(234,534);
-		GeometricObject o3 = new GeometricObject(123);
-		GeometricObject o4 = new GeometricObject();
-		
+
 		System.out.println(o1.equals(o2));
 		o2.moveTo(42,23);
 		System.out.println(o1.equals(o2));
