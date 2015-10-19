8ae9b8a02f93f4f1a3090ec6651c760af79bf814
weisnich <jochenruf98@web.de>
2015-10-19 21:00:16 +0200

    Update Square.java

--- animation game/src/Square.java
+++ animation game/src/Square.java
@@ -1,7 +1,22 @@
 
 public class Square extends GeometricObject{
+	
 
-	public Square(double w, double h){
+		public Square(double w, Vertex pos){
+			super(w,w,pos);
+		}
 		
+		public Square(double w, double x, double y){
+			super(w,w,new Vertex(x,y));
+		}
+		@Override public double area(){return width*width;}
+
+		public @Override String toString(){
+			return "Square("+super.toString()+")";
+		}
+		
+		public @Override boolean equals(Object that){
+			return (that instanceof Square) && super.equals(that);
+		}
 	}
-}
+
