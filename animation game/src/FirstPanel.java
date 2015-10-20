7a1a98e72806686540b443221750775b07948a6f
Ruf.Jochen <Ruf.Jochen@SK-448A5BCB8A89>
2015-10-20 15:17:07 +0200

    Update 20.10.2015

--- animation game/src/FirstPanel.java
+++ animation game/src/FirstPanel.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 public class  FirstPanel extends JPanel {
 	@Override public void paintComponent(java.awt.Graphics g) {
-	g.fillRect(40, 30, 40, 23);			
+	g.fillOval(200, 200, 500, 500);	
+	g.fillRect(343, 111, 300, 123);
 	}
 	public static void main(String[] args) {
 		JFrame f=new JFrame();
 		f.add(new FirstPanel());
 		f. setVisible (true) ;
+		f.setSize(1000,1000);
 	}
 }
